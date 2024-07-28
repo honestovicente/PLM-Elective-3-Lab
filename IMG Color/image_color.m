d772b1e24e7de3f56ffdf097451e1a532f9f2881
Honesto Vicente <hevicente2021@plm.edu.ph>
2024-07-28 18:42:54 +0800

    Add files via upload

---
+++ IMG Color/image_color.m
@@ -0,0 +1,27 @@
+img_color = imread('C:\Users\user\Downloads\orange.jpg');
+
+% Red
+img_red = img_color;
+img_red(:,:,2) = 0;
+img_red(:,:,3) = 0;
+figure(1);
+imshow(img_red);
+
+% Green
+img_green = img_color;
+img_green(:,:,1) = 0;
+img_green(:,:,3) = 0;
+figure(2);
+imshow(img_green);
+
+% Blue
+img_blue = img_color;
+img_blue(:,:,1) = 0;
+img_blue(:,:,2) = 0;
+figure(3);
+imshow(img_blue);
+
+% Gray
+g = rgb2gray(img_color);
+figure(4);
+imshow(g);
